f283475c747f5e9703f838a06d460b4891242b52
Mitchell Glazier <mitchell.glazier@telenotes.com>
2019-08-13 17:59:41 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OwnersComponent } from './owners/owners.component';
+import { DraftComponent } from './draft/draft.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    OwnersComponent,
+    DraftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'owners', component: OwnersComponent},
+      { path: 'draft', component: DraftComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
